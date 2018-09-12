4faf9ac57a2bd714adca376d3837728f494e5626
Mitchell Lisle <mitchell.lisle@bigdatr.com>
2018-09-12 14:48:55 +1000

    added retries for lambda

--- termatopy/aws.py
+++ termatopy/aws.py
@@ -166,7 +166,7 @@ def extractBucketName(location):
 def invokeLambda(accessKey, secret, arn, payload, region = 'ap-southeast-2', maxRetries = 0):
     try:
         cfg = botocore.config.Config(retries={'max_attempts': maxRetries})
-        client = boto3.client('lambda', aws_access_key_id = accessKey, aws_secret_access_key = secret, region_name = region)
+        client = boto3.client('lambda', aws_access_key_id = accessKey, aws_secret_access_key = secret, region_name = region, config=cfg)
         res = client.invoke(FunctionName = arn, Payload = json.dumps(payload), InvocationType = 'Event')
         return res
     except Exception as e:
