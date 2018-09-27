4a80a7640ad961189e99fadba58ff8f9d6b9b1a6
mikelimbda <michael.lim@bigdatr.com>
2018-09-27 11:57:29 +1000

    add test

---
+++ tests/test_ftp.py
@@ -0,0 +1,14 @@
+from unittest import TestCase
+from unittest.mock import MagicMock
+from unittest.mock import Mock
+import ftplib
+
+
+def test_ftpCreateInstance():
+    mock_ftp_instance = Mock()
+    ftplib.FTP = MagicMock(return_value=mock_ftp_instance)
+    actual = ftpCreateInstance('host', 'user', 'passwd')
+    assert mock_ftp_instance == actual
+
+
+
