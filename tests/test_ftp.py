ed6613a59b7b1f73c266530ec7af24dfee23f199
Michael Lim <michael.lim@bigdatr.com>
2018-09-27 12:07:47 +1000

    Update test_ftp.py

--- tests/test_ftp.py
+++ tests/test_ftp.py
@@ -2,7 +2,7 @@ from unittest import TestCase
 from unittest.mock import MagicMock
 from unittest.mock import Mock
 import ftplib
-import termatopy as tpy
+from termatopy.ftp import ftpCreateInstance
 
 
 def test_ftpCreateInstance():
