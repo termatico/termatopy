01fb7c219827f048d26b55f42b5f8a372ff7c047
mikelimbda <michael.lim@bigdatr.com>
2018-10-02 14:21:54 +1000

    add ftpListFiles test

--- tests/test_ftp.py
+++ tests/test_ftp.py
@@ -3,6 +3,7 @@ from unittest.mock import MagicMock
 from unittest.mock import Mock
 import ftplib
 from termatopy.ftp import ftpCreateInstance
+from termatopy.ftp import ftpListFiles
 
 
 def test_ftpCreateInstance():
@@ -10,3 +11,13 @@ def test_ftpCreateInstance():
     ftplib.FTP = MagicMock(return_value=mock_ftp_instance)
     actual = ftpCreateInstance('host', 'user', 'passwd')
     assert mock_ftp_instance == actual
+
+def test_ftpListFiles():
+    mock_ftp_object = Mock()
+    mock_list = Mock()
+
+    ftplib.FTP = MagicMock(return_value=mock_ftp_object)
+    mock_ftp_object.nlst = MagicMock(return_value=mock_list)
+    actual = ftpListFiles(mock_ftp_object, 'directory')
+
+    assert mock_list == actual
