e18e9a45a1fd2e801a0a87df247e72e35e066154
Mitchell Lisle <mitchell.lisle@bigdatr.com>
2018-12-03 13:39:22 +1100

    add token fetch

--- termatopy/__init__.py
+++ termatopy/__init__.py
@@ -44,10 +44,7 @@ def adapt_numpy_int64(numpy_int64):
     return AsIs(numpy_int64)
 
 
-register_adapter(numpy.int64, adapt_numpy_int64)
-
-import numpy
-from psycopg2.extensions import register_adapter, AsIs
+register_adapter(np.int64, adapt_numpy_int64)
 
 
 def adapt_numpy_bool(numpy_bool):
@@ -58,5 +55,25 @@ def adapt_numpy_bool(numpy_bool):
     """
     return AsIs(numpy_bool)
 
+register_adapter(np.bool_, adapt_numpy_bool)
 
-register_adapter(numpy.bool_, adapt_numpy_bool)
+def adapt_none(NoneType):
+    """ Adapting numpy.bool_ type to SQL-conform int type using psycopg extension, see [1]_ for more info.
+    References
+    ----------
+    .. [1] http://initd.org/psycopg/docs/advanced.html#adapting-new-python-types-to-sql-syntax
+    """
+    return AsIs(numpy_bool)
+
+
+register_adapter(np.bool_, adapt_numpy_bool)
+
+def nan_to_null(f,
+        _NULL=ps.extensions.AsIs('NULL'),
+        _NaN=np.NaN,
+        _Float=ps.extensions.Float):
+    if f is not _NaN:
+        return _Float(f)
+    return _NULL
+
+register_adapter(float, nan_to_null)
